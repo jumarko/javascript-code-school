12b71a9cff47ff63f09154bac729274ab8820fa4
Juraj Martinka <juraj@snapshot.travel>
2016-12-23 23:08:47 +0100

    Add examples for JavaScript Road Trip Part 1

---
+++ javascript-road-trip/javascript-road-trip-1/resources/js/level3/trains.js
@@ -0,0 +1,16 @@
+// Goal: Print list of trains which are running.
+var totalTrains = 12;
+var trainsOperational = 8;
+console.log("There are " + trainsOperational + " running trains.");
+
+console.log("Train #" + 1 + " is running.")
+console.log("Train #" + 2 + " is running.")
+console.log("Train #" + 3 + " is running.")
+console.log("Train #" + 4 + " is running.")
+console.log("Train #" + 5 + " is running.")
+console.log("Train #" + 6 + " is running.")
+console.log("Train #" + 7 + " is running.")
+console.log("Train #" + 8 + " is running.")
+
+
+
